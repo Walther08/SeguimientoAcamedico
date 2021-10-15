f2ade21697ea24e7644507deb535c1af4c41638e
Walther08 <gonzalezwalther08@gmail.com>
2021-10-15 00:08:29 -0500

    Register, Login y Logout funcionales.

--- SeguiAcademico.App/SeguiAcademico.App.Presentacion/Startup.cs
+++ SeguiAcademico.App/SeguiAcademico.App.Presentacion/Startup.cs
@@ -30,6 +30,7 @@ namespace SeguiAcademico.App.Presentacion
             services.AddSingleton<IRepositorioTutor, RepositorioTutor>();
             services.AddSingleton<IRepositorioAcudienteDesignado, RepositorioAcudienteDesignado>();
             services.AddSingleton<IRepositorioHistorial, RepositorioHistorial>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,8 @@ namespace SeguiAcademico.App.Presentacion
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
